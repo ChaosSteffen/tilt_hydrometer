69913463d7522dc0dd559e7de151ef69f016d095
Steffen Schröder <steffen.schroeder@aklamio.com>
2023-04-20 11:02:44 +0200

    show line coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start do
   track_files 'lib/**/*.rb'
 end
